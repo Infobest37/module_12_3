6fa6ed8f9e1cd3e7aee2a89a4918be750e6e4c8f
Евгений <info-best@bk.ru>
2024-11-07 12:55:03 +0300

    second1

--- Module_12_3.py
+++ Module_12_3.py
@@ -1,7 +1,10 @@
 import unittest
 import Module_12_2
+import Unut_Test_Runner
 calcST = unittest.TestSuite()
 calcST.addTest(unittest.TestLoader().loadTestsFromTestCase(Module_12_2.Tournament1))
+calcST.addTest(unittest.TestLoader().loadTestsFromTestCase(Unut_Test_Runner.RunnerTest))
+
 
 
 runner = unittest.TextTestRunner(verbosity=2)
---
+++ Unut_Test_Runner.py
@@ -0,0 +1,39 @@
+import unittest
+from Test_Runner import Runner
+
+class RunnerTest(unittest.TestCase):
+    def setUp(self):
+        print('setUp')
+
+    @classmethod
+    def tearDownClass(cls):
+        print('setUpClass')
+    @unittest.skipIf(True, "Тесты в этом кейсе заморожены'")
+    def test_wallk(self):
+
+        runner = Runner('wallk')
+
+        for _ in range(10):
+            runner.walk()
+        self.assertEqual(50, runner.distance)
+    @unittest.skipIf(True, "Тесты в этом кейсе заморожены'")
+    def test_run(self):
+        runner = Runner('run')
+
+        for _ in range(10):
+            runner.run()
+        self.assertEqual(100, runner.distance)
+    @unittest.skipIf(True, "Тесты в этом кейсе заморожены'")
+    def test_challenge(self ):
+        runner1 = Runner('challenge')
+        runner2 = Runner('challenge2')
+        for _ in range(10):
+            runner1.walk()
+        for _ in range(10):
+            runner2.run()
+        self.assertNotEquals(runner2.distance, runner1.distance)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
